35f587f4f49e868605cb3e78c7417508b809404d
Tristan Fisher <tfisher@singleplatform.com>
2015-06-18 18:44:09 -0400

    needed for new config import loading

---
+++ config_plugins.py
@@ -0,0 +1,9 @@
+# import the secrets file that contains 3rd party auth tokens, etc.
+import secrets
+
+class AWS:
+
+    # not implemented
+    AWS_ACCESS_KEY_ID = ''
+    AWS_SECRET_ACCESS_KEY = ''
+    REGION = 'us-east-1'
