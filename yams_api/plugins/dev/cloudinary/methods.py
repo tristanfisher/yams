ee583ab70546769c1742a15254b36e7df2d5485a
Tristan Fisher <tfisher@singleplatform.com>
2015-12-02 16:40:08 -0500

    parameterizes url for cloudinary status

--- yams_api/plugins/dev/cloudinary/methods.py
+++ yams_api/plugins/dev/cloudinary/methods.py
@@ -1,7 +1,8 @@
 from lxml.html import fromstring as html_string
 from yams_api.plugins.dev.api_utils.http_methods import http_requests_get_to_dict
 
-def get_cloudinary_status():
+
+def get_cloudinary_status(url="http://status.cloudinary.com"):
 
     # this is only history -- no "we're fine right now" or enumeration of things that are working.
     # http://status.cloudinary.com/history.rss
@@ -9,7 +10,7 @@ def get_cloudinary_status():
     # makes this generally useless for our purposes.
 
 
-    r = http_requests_get_to_dict("http://status.cloudinary.com")
+    r = http_requests_get_to_dict(url)
 
     # response dict - response is data blob to return (e.g. 'Site is normal!'), headers are headers, status is ok/nok
     _resp = {
@@ -45,6 +46,9 @@ def get_cloudinary_status():
             _inner_div_group = _service.xpath("div[contains(@class, 'component-inner-container')]")[0]
             _name = _inner_div_group.xpath("span[@class='name']")[0].text.strip()
             _status = _inner_div_group.xpath("span[@class='component-status']")[0].text.strip()
+            if _status == "Operational":
+                _status = "ok"
+
             _serv_status[_name] = _status
 
         _resp["response"]["services"] = _serv_status
